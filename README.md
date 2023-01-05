8017dfd98e53e7c149ee056c8c02e63a0abeb77d
ilsuryusipov <->
2023-01-06 02:54:26 +0300

    task add & backlog show

--- README.md
+++ README.md
@@ -1,2 +1,44 @@
 # antiburner-bot
 Telegram bot for antiburner
+
+
+## python-telegram-bot latest version package installation
+
+```bash
+virtualenv -p python3 venv
+source venv/bin/activate
+git clone https://github.com/python-telegram-bot/python-telegram-bot
+cd python-telegram-bot/
+python setup.py install
+```
+
+## python-telegram-bot usage examples
+
+```python
+chat_id = update.message.chat_id
+# chat_id = update.effective_chat.id
+received_message_id = update.message.message_id
+```
+
+```python
+send_message = await context.bot.send_message(
+	chat_id=update.effective_chat.id,
+	text="text of message",
+	parse_mode=ParseMode.MARKDOWN_V2
+)
+```
+
+```python
+await context.bot.delete_message(
+	chat_id=update.message.chat_id,
+	message_id=update.message.message_id
+)
+```
+
+```python
+await context.bot.edit_message_text(
+	text="new text of message",
+	chat_id=update.effective_chat.id,
+	message_id=m_id # from store
+)
+```
--- bot.py
+++ bot.py
@@ -1,24 +1,125 @@
-from telegram import Update
-from telegram.ext import Updater
-from telegram.ext import CallbackContext
-from telegram.ext import Filters
-from telegram.ext import MessageHandler
 import logging
+import sqlite3
+from dataclasses import dataclass
+from telegram import Update
+from telegram.constants import ParseMode
+from telegram.ext import ApplicationBuilder
+from telegram.ext import ContextTypes
+from telegram.ext import CommandHandler
+from telegram.ext import MessageHandler
+from telegram.ext import filters
 
 import settings
 
+logging.basicConfig(
+	format='[%(asctime)s] %(levelname)s: %(message)s',
+	level=logging.INFO
+)
 
-logging.basicConfig(format='%(asctime)s [%(levelname)s] %(message)s',
-                    level=logging.INFO)
+
+@dataclass
+class Task:
+	id: int
+	text: str
 
 
-def common_answer(update: Update, context: CallbackContext) -> None:
-    logging.info(f"from chat {update.effective_chat.id} message: {update.message.text}")
-    context.bot.send_message(chat_id=update.effective_chat.id, text="pong")
+async def _is_authorized(update: Update, context: ContextTypes.DEFAULT_TYPE) -> bool:
+	if update.effective_user.id in settings.ALLOWED_USERS:
+		return True
 
-message_handler = MessageHandler(Filters.text, common_answer)
+	# await context.bot.send_message(
+	# 	text=f"Ты не авторизован\!",
+	# 	chat_id=update.message.chat_id,
+	# 	parse_mode=ParseMode.MARKDOWN_V2
+	# )
+	logging.warning(f"Not authorized user {update.effective_user.id}")
+	return False
 
-updater = Updater(token=settings.TOKEN)
-dispatcher = updater.dispatcher
-dispatcher.add_handler(message_handler)
-updater.start_polling()
+
+def _prepare_db():
+	con = sqlite3.connect(settings.SQLITE3_DB_NAME)
+	cur = con.cursor()
+	cur.execute("CREATE TABLE if not exists backlog(id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, text TEXT)")
+
+
+def _add_task(task: Task) -> int:
+	con = sqlite3.connect(settings.SQLITE3_DB_NAME)
+	cur = con.cursor()
+	query = f"INSERT INTO backlog(text) VALUES('{task.text}')"
+	logging.info(f"{query=}")
+	res = cur.execute(query)
+	logging.info(f"New record with id {res.lastrowid} added")
+	con.commit()
+	return res.lastrowid
+
+
+def _receive_tasks() -> list[Task]:
+	con = sqlite3.connect(settings.SQLITE3_DB_NAME)
+	cur = con.cursor()
+	rows = cur.execute("SELECT id, text FROM backlog")
+	return [Task(row[0], row[1]) for row in rows.fetchall()]
+
+
+def _format_backlog(tasks: list[Task]) -> str:
+	backlog = ["*BACKLOG:*\n"]
+	for task in tasks:
+		postfix = ""
+		text = task.text
+		if len(text.split("\n")) > 1:
+			postfix = "..."
+			text = text.split("\n")[0]
+		if len(text) > 70:
+			postfix = "..."
+			text = text[:70]
+		backlog.append(f"\[{task.id}] {text+postfix}\n")
+	return "".join(backlog)
+
+
+async def backlog_command_handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
+	chat_id = update.message.chat_id
+	user_id = update.effective_user.id
+	received_message_id = update.message.message_id
+
+	if not await _is_authorized(update, context):
+		return
+
+	formated_backlog = _format_backlog(_receive_tasks())
+	logging.info(f"{formated_backlog=}")
+
+	await context.bot.send_message(
+		text=formated_backlog,
+		chat_id=chat_id,
+		parse_mode=ParseMode.MARKDOWN,
+		disable_web_page_preview=True
+	)
+
+
+async def message_handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
+	chat_id = update.message.chat_id
+	user_id = update.effective_user.id
+	username = update.effective_user.name
+	received_message_text = update.message.text
+
+	if not await _is_authorized(update, context):
+		return
+
+	logging.info(f"Message from {user_id=} {username=}")
+	logging.info(f"Data: {received_message_text}")
+
+	task_id = _add_task(Task(None, received_message_text))
+
+	await context.bot.send_message(
+		text=f"✅ Task [{task_id}] added!",
+		chat_id=chat_id,
+		parse_mode=ParseMode.MARKDOWN
+	)
+
+
+if __name__ == '__main__':
+	_prepare_db()
+	application = ApplicationBuilder().token(settings.TOKEN).build()
+
+	application.add_handler(CommandHandler('backlog', backlog_command_handler))
+	application.add_handler(MessageHandler(filters.TEXT, message_handler))
+
+	application.run_polling()
